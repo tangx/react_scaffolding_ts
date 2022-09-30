b419b4d4abc6f0d1130cbd95308a7609dee75ea4
Frank (Xin) Tang <f.tang@aligntech.com>
2022-10-01 00:18:00 +0800

    2.4 people 增加 UI

--- src/components/People/index.tsx
+++ src/components/People/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import { connect } from 'react-redux'
 import { rootStateType } from '../../redux/store'
+
+import { nanoid } from 'nanoid'
 
 import { ICount } from '../../redux/reducers/count'
 import { IPeopleState, IPerson } from '../../redux/reducers/people'
@@ -12,12 +14,34 @@ interface PeopleProps {
 }
 
 function People(props: PeopleProps) {
+
+  const nameNode = createRef<HTMLInputElement>()
+  const ageNode = createRef<HTMLInputElement>()
+
+  const addPerson = () => {
+    // const name = (nameNode.current as HTMLInputElement).value
+    const name = nameNode.current!.value
+    const age = ageNode.current!.valueAsNumber
+
+    const p: IPerson = {
+      id: nanoid(),
+      name,
+      age,
+    }
+
+    // console.log(p);
+
+  }
+
   return (
     <div>
       <h1>2. People 组件, 总人数: {props.people.length}</h1>
       <h2>Count 组件的和： {props.count} </h2>
       <h3>用户列表</h3>
-
+      <input type="text" placeholder='用户名' ref={nameNode} />
+      {/* 注意这里年龄 input 的 type 是 number */}
+      <input type="number" placeholder='年纪' ref={ageNode} />
+      <button onClick={addPerson}>添加用户</button>
       <ul>
         {
           props.people.map(

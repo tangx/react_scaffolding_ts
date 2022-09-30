5634a02b1dbf6cdea669430a144e861979f23ca4
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-30 23:10:06 +0800

    Count 页面布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Count from './components/Count';
 
 function App() {
 
   return (
     <div className="App">
       <h1> react-redux-ts: people-and-count</h1>
+      <hr />
+      <Count />
       <hr />
     </div>
   );
---
+++ src/components/Count/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Count() {
+  return (
+    <div>
+      <h2>1. Count 组件</h2>
+      <h3>求和总数为: ???/</h3>
+      <button>再 +1</button>
+      <button>和为奇数 +1</button>
+      <button>延迟 +1</button>
+    </div>
+  )
+}

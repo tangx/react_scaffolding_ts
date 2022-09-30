893d8f4695db75a0412d772465bba9215d17b813
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-30 15:16:13 +0800

    启用 redux 开发者工具

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 
-
-const store = createStore(reducers)
+const middlewares = applyMiddleware()
+const store = createStore(reducers, composeWithDevTools(middlewares))
 
 export default store
